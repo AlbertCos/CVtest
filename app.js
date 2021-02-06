20767b78ef7f3de5a12c9f25e823cad53eece5ce
AlbertCos <cosmenalberto@gmail.com>
2021-02-06 13:29:54 +0100

    Project todolist1

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const bodyparser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req,res){
-  res.send("hello")
+app.get("/", function(req, res) {
+
+  var today = new Date();
+  if (today.getDay() === 6 || today.getDay() === 0) {
+  res.send("Yay it´s the weekend!");
+} else {
+  res.send("Boo! I have to work!");
+}
+
 });
 
 
 
-app.listen("3000", function(){
+app.listen("3000", function() {
   console.log("Port: 3000 connected!")
 });
